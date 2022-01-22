78d8c6bc8f4c5c707be1a3ea3eace3541eb6577f
zkno <zkno@MBP14inch2021.fibertel.com.ar>
2022-01-22 16:51:44 -0300

    delete functionality and refresh table in lwcAvailableProducts

--- force-app/main/default/lwc/lwcOrderProducts/lwcOrderProducts.js
+++ force-app/main/default/lwc/lwcOrderProducts/lwcOrderProducts.js
@@ -1,4 +1,8 @@
 import { LightningElement, api, track, wire } from 'lwc';
+import { updateRecord } from 'lightning/uiRecordApi';
+import getXLineItems from '@salesforce/apex/LWCOrderProductsController.getXLineItems';
+import handleXLineItemUpdate from '@salesforce/apex/LWCOrderProductsController.handleXLineItemUpdate';
+
 import {
     APPLICATION_SCOPE,
     createMessageContext,
@@ -12,10 +16,19 @@ import updateXLineItems from '@salesforce/messageChannel/UpdateXLineItems__c';
 
 const columns = [
     { label: 'Product', type: 'String', fieldName: 'productName', sortable:true},
-    { label: 'Unit Price', type: 'currency', fieldName: 'unitPrice', sortable:true},
-    { label: 'Quantity', type: 'number', fieldName: 'quantity', sortable:true},
-    { label: 'Total Price', type: 'currency', fieldName: 'TotalPrice', sortable:true},
-    { label: '',title:'title', name:'name', type: 'button'}
+    { label: 'Unit Price', type: 'currency', fieldName: 'unitPrice', sortable:true,
+        cellAttributes: { class: 'slds-text-align_right'}
+    },
+    { label: 'Quantity', type: 'number', fieldName: 'quantity',
+        cellAttributes: { alignment: 'center'}
+    },
+    { label: 'Total Price', type: 'currency', fieldName: 'totalPrice', sortable:true,
+        cellAttributes: { class: 'slds-text-align_right'}
+    },
+    { label: '', name: 'delete', type: 'button', 
+        typeAttributes: { label: 'Delete'},
+        cellAttributes: { class: 'slds-align_absolute-center'}
+    }
 ];
 
 export default class LwcOrderProducts extends LightningElement {
@@ -24,6 +37,48 @@ export default class LwcOrderProducts extends LightningElement {
     @track data = [];
     columns = columns;
 
+    connectedCallback(){
+        this.getXLineItemsFromApex();
+    }
+
+    getXLineItemsFromApex(){
+        getXLineItems(
+            {
+                parentName : this.parentName,
+                parentId: this.recordId
+            }
+        )
+        .then(data => {
+            this.data = data;
+        })
+        .catch(error => {
+            console.log('error:', error);
+        })
+
+    }
+
+    handleRowAction(event){
+        this.handleXLineItemUpdateFromApex(event.detail.row.xliId);
+    }
+
+    handleXLineItemUpdateFromApex(xliId){
+        handleXLineItemUpdate(
+            {
+                parentName: this.parentName,
+                xliId
+            }
+        )
+        .then(() => {
+            console.log('xliId deleted REFRESH!');
+            this. data = [];
+            this.getXLineItemsFromApex();
+        })
+    }
+
+    refreshTableAndPage(){
+        this.getXLineItemsFromApex(); 
+        updateRecord({ fields: { Id: this.recordId } });       
+    }
 
     sortBy(field, reverse, primer) {
         const key = primer
